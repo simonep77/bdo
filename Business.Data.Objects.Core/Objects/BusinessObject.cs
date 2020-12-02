490aad566b8f69ea03b2932f1dfd8aa3f35bd9ff
Simone Pelaia <simone.pelaia@casagit.eu>
2020-12-02 18:39:32 +0100

    Lazy load semplificato su business object

--- Business.Data.Objects.Core/Objects/BusinessObject.cs
+++ Business.Data.Objects.Core/Objects/BusinessObject.cs
@@ -1,5 +1,6 @@
 using Business.Data.Objects.Common.Exceptions;
 using Business.Data.Objects.Core.Base;
+using System.Collections.Generic;
 
 namespace Business.Data.Objects.Core
 {
@@ -41,9 +42,37 @@ namespace Business.Data.Objects.Core
         }
 
 
-        #region STATIC
+        #region LAZY LOAD MANAGEMENT
 
-  
+
+        private Dictionary<string, object> mDic = new Dictionary<string, object>();
+
+        /// <summary>
+        /// Funzione di caricamento oggetto lazy
+        /// </summary>
+        /// <returns></returns>
+        protected delegate object LazyLoadFunc();
+
+        /// <summary>
+        /// Ritorna oggetto precedentemente caricato oppure lo carica tramite la funzione in input e lo memorizza per accessi successivi
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="propertyName"></param>
+        /// <param name="fn"></param>
+        /// <returns></returns>
+        protected T GetLazy<T>(string propertyName, LazyLoadFunc fn)
+        {
+            object obj;
+
+            if (!mDic.TryGetValue(propertyName, out obj))
+            {
+                obj = fn();
+                mDic.Add(propertyName, obj);
+            }
+
+            return (T)obj;
+        }
+
 
         #endregion
 
